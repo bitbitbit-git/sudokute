30dc4066e17606a7ccc390de2d1c6eebdaba64b1
_bitbitbit_ <127902612+bitbitbit-git@users.noreply.github.com>
2023-10-15 22:40:09 -0300

    Delete _sudokute/_sudokute directory

--- _sudokute/_sudokute/scenes/game/grid_button.gd
+++
@@ -1,7 +0,0 @@
-extends TextureButton
-
-
-# Called when the node enters the scene tree for the first time.
-func update_number(number):
-	get_node("Number").visible = true
-	get_node("Number").frame = number-1
